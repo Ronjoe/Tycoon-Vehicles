22220c37d69a4299b9152136745ece6e0844d14a
Ronjoe <rjr13d@my.fsu.edu>
2017-11-19 23:43:58 -0500

    Consolidated into fewer resources

--- addon-cars/vehicle_names.lua
+++ addon-cars/vehicle_names.lua
@@ -6,6 +6,6 @@ Citizen.CreateThread(function()
 
 AddTextEntry('0x3193A0F1', 'polgs350')
 AddTextEntry('0xCA2D4880', 'rc350')
-AddTextEntry('0xF345AE2D', 'rcf')
+AddTextEntry('0xE3DD5188', 'swatbrickade')
 
 end)
--- planes/vehicle_names.lua
+++
@@ -1,9 +0,0 @@
-function AddTextEntry(key, value)
-	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
-end
-
-
-
-Citizen.CreateThread(function()
-    AddTextEntry('0xB8D2287F', 'osprey')
-end)
