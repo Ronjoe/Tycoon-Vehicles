4a28809847cd4a811e5732ca2d8f86514090c905
Ronjoe <rjr13d@my.fsu.edu>
2017-11-20 22:29:22 -0500

    added entries to vehicle_names.lua (for both resources)

--- addon-aircraft/vehicle_names.lua
+++ addon-aircraft/vehicle_names.lua
@@ -5,5 +5,10 @@ end
 
 
 Citizen.CreateThread(function()
+    AddTextEntry('0xE66F744D', '739')
+    AddTextEntry('0xCFEA9D59', 'dc10f')
+    AddTextEntry('0x9CEB94D6', 'emb390fedex')
     AddTextEntry('0xB8D2287F', 'osprey')
+    AddTextEntry('0x842B99F0', 'uh1nasa')
+
 end)
--- addon-cars/vehicle_names.lua
+++ addon-cars/vehicle_names.lua
@@ -4,8 +4,13 @@ end
 
 Citizen.CreateThread(function()
 
-AddTextEntry('0x3193A0F1', 'polgs350')
-AddTextEntry('0xCA2D4880', 'rc350')
-AddTextEntry('0xE3DD5188', 'swatbrickade')
+    AddTextEntry('0x3193A0F1', 'polgs350')
+    AddTextEntry('0xCA2D4880', 'RC350')
+    AddTextEntry('0x0394D475', 'sectruck')
+    AddTextEntry('0xE7FF77DB', 'security1')
+    AddTextEntry('0xBA9C1D0D', 'security2')
+    AddTextEntry('0xCB55BE80', 'security3')
+    AddTextEntry('0xE3DD5188', 'swatbrickade')
+    AddTextEntry('0x6B8645E6', 'urnext')
 
 end)
